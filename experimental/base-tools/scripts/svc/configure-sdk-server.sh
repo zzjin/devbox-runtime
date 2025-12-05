20526f3c1baacec703b4217c5cb0a222c1b0088c
zzjin <tczzjin@gmail.com>
2025-12-05 14:04:09 +0800

    feat: add sdk-server configuration and installation to service setup (#3)
    * feat: add sdk-server configuration and installation to service setup
    
    Signed-off-by: zzjin <tczzjin@gmail.com>
    
    * fix: update environment variable references in sdk server scripts for consistency
    
    Signed-off-by: zzjin <tczzjin@gmail.com>
    
    ---------
    
    Signed-off-by: zzjin <tczzjin@gmail.com>

--- experimental/base-tools/scripts/svc/configure-sdk-server.sh
+++ experimental/base-tools/scripts/svc/configure-sdk-server.sh
@@ -10,17 +10,15 @@ S6_DIR=/etc/s6-overlay/s6-rc.d
 SDK_SERVER=sdk-server
 mkdir -p "$S6_DIR/$SDK_SERVER" "$S6_DIR/$SDK_SERVER/dependencies.d"
 
-
-
 # Create run first
 echo longrun >"$S6_DIR/$SDK_SERVER/type"
 cat >"$S6_DIR/$SDK_SERVER/run" <<'sdk-server'
 #!/command/with-contenv bash
 set -euo pipefail
 
-if [ -n "${SEALOS_DEVBOX_JWT_SECRET:-}" ] && [ "$DEVBOX_ENV" != "production" ]; then
-	# SEALOS_DEVBOX_JWT_SECRET exists and is non-empty AND DEVBOX_ENV is not production
-	# start the longrun service.
+if [ -n "${DEVBOX_JWT_SECRET:-}" ] && [ "${DEVBOX_ENV:-}" != "production" ]; then
+	echo "DEVBOX_JWT_SECRET exists and is non-empty AND DEVBOX_ENV is not production"
+	# start the longrun devbox sdk server service.
 	exec /usr/sbin/devbox-sdk-server --workspace-path=/home/devbox/project
 else
 	# custom exit code to indicate missing env var
@@ -35,7 +33,7 @@ cat >"$S6_DIR/$SDK_SERVER/finish" <<'finish-sdk-server'
 #!/usr/bin/env bash
 EXIT_CODE=$1
 if [ "$EXIT_CODE" = "101" ]; then
-	echo "Devbox SDK Server stopped due to missing required environment variable SEALOS_DEVBOX_JWT_SECRET. Preventing s6 restart."
+	echo "Devbox SDK Server stopped due to missing required environment variable DEVBOX_JWT_SECRET. Preventing s6 restart."
 	# s6 docs: Exiting finish script with 125 tells s6 to stop managing the service.
 	exit 125
 else
